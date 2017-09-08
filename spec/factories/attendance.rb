14952422e3e33e9b96c2be228b5e6735c824f799
nekosuke0728 <hatarakazarumono.kuubekarazu00@gmail.com>
2017-09-08 23:00:34 +0900

    “issue#8.7_complete”

--- spec/factories/attendance.rb
+++ spec/factories/attendance.rb
@@ -38,4 +38,39 @@ FactoryGirl.define do
     end_at nil 
   end
 
+  trait :attendance_date1 do
+    id 8
+    user_id 1
+    start_at Time.zone.local(2016, 8, 1, 9, 00, 00).to_s
+    end_at Time.zone.local(2016, 8, 1, 17, 00, 00).to_s
+  end
+
+  trait :attendance_date2 do
+    id 9
+    user_id 1
+    start_at Time.zone.local(2016, 8, 2, 9, 00, 00).to_s
+    end_at Time.zone.local(2016, 8, 2, 17, 00, 00).to_s
+  end
+
+  trait :attendance_date3 do
+    id 10
+    user_id 1
+    start_at Time.zone.local(2016, 8, 3, 9, 00, 00).to_s
+    end_at Time.zone.local(2016, 8, 3, 17, 00, 00).to_s
+  end
+
+  trait :attendance_date4 do
+    id 11
+    user_id 1
+    start_at Time.zone.local(2016, 8, 4, 9, 00, 00).to_s
+    end_at Time.zone.local(2016, 8, 4, 17, 00, 00).to_s
+  end
+
+  trait :attendance_date5 do
+    id 12
+    user_id 1
+    start_at Time.zone.local(2016, 8, 5, 9, 00, 00).to_s
+    end_at Time.zone.local(2016, 8, 5, 17, 00, 00).to_s
+  end
+
 end
